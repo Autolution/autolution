2d70a8a69167c5934568e37bd63bd984a8b404ed
Hyderite <hyderiteyt@gmail.com>
2023-08-18 00:19:55 +0800

    Made a basic animation for about us page

---
+++ public/js/about-us.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+    setTimeout(function () {
+        document.querySelector("#one-title").style.transform = "translateY(-30px)";
+        document.querySelector("#down-one").style.transform = "translateY(30px)";
+        document.querySelector("#down-one").style.display = "block";
+        setTimeout(function () {
+            document.querySelector("#down-one").style.opacity = "1";
+            document.querySelector("#down-one").style.animation = "scroll-down 2.5s";
+            document.querySelector("#down-one").style.animationIterationCount = "infinite";
+        }, 500);
+    }, 1500);
+    setTimeout(function () {
+        document.querySelector("#two").style.transition = "transform .75s";
+        document.querySelector("#three").style.transition = "transform .75s";
+    }, 300);
+};
+
+document.querySelector("#one").addEventListener("mousewheel", function (event) {
+    if (event.deltaY < 0) {
+        alert("top");
+    } else if (event.deltaY > 0) {
+        document.querySelector("#two").style.transform = "translateY(-110px)";
+    };
+});
+
+document.querySelector("#two").addEventListener("mousewheel", function (event) {
+    if (event.deltaY < 0) {
+        document.querySelector("#two").style.transform = "translateY(100%)";
+    } else if (event.deltaY > 0) {
+        document.querySelector("#three").style.transform = "translateY(-110px)";
+    };
+});
+
+document.querySelector("#three").addEventListener("mousewheel", function (event) {
+    if (event.deltaY < 0) {
+        document.querySelector("#three").style.transform = "translateY(100%)";
+    } else if (event.deltaY > 0) {
+        alert("bottom");
+    };
+});
