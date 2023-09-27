b358419f80e9a0d646a6c53243b323da94550245
Hyderite <hi@hyderite.dev>
2023-09-27 19:16:46 +0800

    Remove alert in about us page

--- public/js/about-us.js
+++ public/js/about-us.js
@@ -49,7 +49,7 @@ if (window.matchMedia("(max-width: 480px)").matches) {
     document.querySelector("#one").ontouchmove = function (event) {
         let newTouchPosition = event.changedTouches[0].clientY;
         if (newTouchPosition > touchPosition) {
-            alert("top");
+            window.reload();
         };
         if (newTouchPosition < touchPosition) {
             document.querySelector("#two").style.transform = "translateY(-110px)";
@@ -72,7 +72,60 @@ if (window.matchMedia("(max-width: 480px)").matches) {
             document.querySelector("#three").style.transform = "translateY(100%)";
         };
         if (newTouchPosition < touchPosition) {
-            alert("bottom");
+
         };
     };
+};
+
+// copied from script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".logo").onmouseover = function () {
+        document.querySelector(".logo-nb-right").style.marginLeft = "191px";
+        setTimeout(function () {
+            document.querySelector("#brand-name-nb").style.visibility = "visible";
+            document.querySelector("#brand-name-nb").style.opacity = "1";
+        }, 300);
+    };
+    document.querySelector(".logo").onmouseout = function () {
+        document.querySelector("#brand-name-nb").style.opacity = "0";
+        setTimeout(function () {
+            document.querySelector("#brand-name-nb").style.visibility = "hidden";
+        }, 300);
+        document.querySelector(".logo-nb-right").style.marginLeft = "22.27px";
+    };
+});
+
+document.querySelector("#mobile-navbar-btn").onclick = function () {
+    document.querySelector("#mobile-navbar-btn").style.opacity = "0";
+    document.querySelector("#mobile-navbar-btn").style.visibility = "hidden";
+    document.querySelector("#mobile-navbar-btn-cross").style.visibility = "visible";
+    document.querySelector("#mobile-navbar-btn-cross").style.opacity = "1";
+    document.querySelector("#navbar").style.height = "120px";
+    document.querySelectorAll("#one, #two, #three").style.transform = "translateY(-118px)";
+    setTimeout(function () {
+        document.querySelector("#mobile-navbar").style.visibility = "visible";
+        document.querySelector("#mobile-navbar").style.opacity = "1";
+    }, 500);
+};
+
+document.querySelector("#mobile-navbar-btn-cross").onclick = function () {
+    document.querySelector("#mobile-navbar-btn-cross").style.opacity = "0";
+    document.querySelector("#mobile-navbar-btn-cross").style.visibility = "hidden";
+    document.querySelector("#mobile-navbar-btn").style.visibility = "visible";
+    document.querySelector("#mobile-navbar-btn").style.opacity = "1";
+    document.querySelector("#mobile-navbar").style.opacity = "0";
+    setTimeout(function () {
+        document.querySelector("#navbar").style.height = "30px";
+        document.querySelector("#mobile-navbar").style.visibility = "hidden";
+        document.querySelectorAll("#one, #two, #three").style.transform = "translateY(0)";
+    }, 500);
+};
+
+document.querySelector("#mobile-navbar").onscroll = function () {
+    document.querySelector("#mobile-navbar-scroll").style.display = "none";
+};
+
+document.querySelector("#mobile-navbar-scroll").onclick = function () {
+    document.querySelector("#mobile-navbar").scrollLeft += 150;
 };
